e5335ecf59386fb670962f8f83ae005f6e03101e
__Rawan__ <rawanroni2003@gmail.com>
2023-08-16 20:27:29 +0300

    upload all project files

---
+++ Task-3/SCRIPTS/script.js
@@ -0,0 +1,40 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(question => {
+  question.addEventListener('click', function handleClick(event) {  
+  txt = question.children[1];
+  console.log(txt)
+  if (txt.style.display === "none") {
+    txt.style.display = "block";
+  } else {
+    txt.style.display = "none";
+  }
+  });
+});
